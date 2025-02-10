a5f8c4ee6431d37946c746f5020875565602f4ee
Josse Van Delm <josse.vandelm@gmail.com>
2025-02-10 12:57:33 +0100

    Make naxirzag-opt a superset of snax-opt (#14)

--- naxirzag/tools/naxirzag_main.py
+++ naxirzag/tools/naxirzag_main.py
@@ -1,10 +1,13 @@
-from xdsl.xdsl_opt_main import xDSLOptMain
+from compiler.tools.snax_opt_main import SNAXOptMain
 
 from naxirzag.transforms import get_all_passes
 
 
-class NaxirzagMain(xDSLOptMain):
+class NaxirzagMain(SNAXOptMain):
     def register_all_passes(self):
+        # Register all snax-opt passes (which includes all xdsl passes)
+        super().register_all_passes()
+        # Aditionally, register all naxirzag passes
         for name, pass_ in get_all_passes().items():
             self.register_pass(name, pass_)
 
--- naxirzag/transforms/__init__.py
+++ naxirzag/transforms/__init__.py
@@ -6,35 +6,11 @@ from xdsl.passes import ModulePass
 def get_all_passes() -> dict[str, Callable[[], type[ModulePass]]]:
     """Returns all available passes."""
 
-    def get_canonicalize():
-        from xdsl.transforms import canonicalize
-
-        return canonicalize.CanonicalizePass
-
-    def get_cse():
-        from xdsl.transforms import common_subexpression_elimination
-
-        return common_subexpression_elimination.CommonSubexpressionElimination
-
-    def get_dce():
-        from xdsl.transforms import dead_code_elimination
-
-        return dead_code_elimination.DeadCodeElimination
-
     def get_linalg_to_stream():
         from naxirzag.transforms import linalg_to_stream
 
         return linalg_to_stream.LinalgToStream
 
-    def get_mlir_opt():
-        from xdsl.transforms import mlir_opt
-
-        return mlir_opt.MLIROptPass
-
     return {
         "linalg-to-stream": get_linalg_to_stream,
-        "canonicalize": get_canonicalize,
-        "cse": get_cse,
-        "dce": get_dce,
-        "mlir-opt": get_mlir_opt,
     }
